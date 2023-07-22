7b8d954aa22eeb51b18af0a4ec688114aaadd999
Yeo Meng Han <yeomenghan1989@gmail.com>
2023-07-23 00:39:36 +0800

    cors problem, troubleshooting http / https

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,9 +3,19 @@ import Login from "./scenes/login";
 import Dashboard from "./pages/Dashboard";
 import { Routes, Route } from 'react-router-dom';
 import { UserContextProvider } from "./services/userContext";
+import { useEffect } from "react";
+
 
 function App() {
- return (
+useEffect(() => {
+  if (window.location.protocol === "https:") {
+    console.log("Vercel is running your app over HTTPS.");
+  } else {
+    console.log("Vercel is running your app over HTTP.");
+  }
+}, []);
+
+  return (
    <div className="App">
      <UserContextProvider>
        <Routes>
