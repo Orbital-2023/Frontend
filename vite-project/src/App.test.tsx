713ec72028ccf1f41a2ffe16a6dbd5d7876aa981
Yeo Meng Han <yeomenghan1989@gmail.com>
2023-07-22 22:24:39 +0800

    deployment ready

--- vite-project/src/App.test.tsx
+++ vite-project/src/App.test.tsx
@@ -1,7 +1,7 @@
 import {render, screen} from "@testing-library/react"
 import App from "./App";
-import Landing from "./pages/Landing";
-import Dashboard from "./pages/Dashboard";
+// import Landing from "./pages/Landing";
+// import Dashboard from "./pages/Dashboard";
 
 describe('Landing Page', () => {
     it("Should have WhenToMeet_NUS",
--- vite-project/src/scenes/login/index.tsx
+++ vite-project/src/scenes/login/index.tsx
@@ -7,7 +7,10 @@ import {useContext, useState} from 'react'
 import { UserContext } from "@/services/userContext";
 
 const Login = () => {
-  const userContext = useContext(UserContext)
+  const userContext = useContext(UserContext);
+  // State to track the error message
+  const [errorMessage, setErrorMessage] = useState("");
+
   const initialValues = {
     roomId: "",
     roomPassword: "",
@@ -18,7 +21,7 @@ const Login = () => {
     roomId: Yup.string().required("Required"),
     roomPassword: Yup.string().required("Required"),
   });
-  
+
   // State to track the loading state
   const [loading, setLoading] = useState(false);
 
@@ -27,12 +30,18 @@ const Login = () => {
     return new Promise((resolve) => setTimeout(resolve, ms));
   };
 
+  // Function to reload the page
+  const reloadPage = () => {
+    window.location.reload();
+  };
+
   const navigate = useNavigate();
 
   const handleSubmit = async (values: any) => {
     try {
       // Start loading state
       setLoading(true);
+      setErrorMessage("");
 
       const response = await axios.post("/api/login", values);
       console.log(values);
@@ -41,7 +50,7 @@ const Login = () => {
         .split(",")
         .map((email: string) => email.trim());
 
-      console.log(emailsArray)
+      console.log(emailsArray);
 
       userContext.setUser({
         roomId: values.roomId,
@@ -52,12 +61,17 @@ const Login = () => {
       setLoading(false);
       console.log(response.data); // Handle the response as needed
       // Add a delay of 3 seconds to help with the backend coming back online
-      // await addDelay(3000);
 
       navigate("/dashboard"); // Redirect to /dashboard upon successful login
+      await addDelay(1000);
     } catch (error) {
-      const errorMessage = (error as any).response?.data?.message || "An error occurred";
-      console.log(errorMessage);    }
+      const errorMessage =
+        (error as any).response?.data?.message || "An error occurred, reloading page";
+      setErrorMessage(errorMessage);
+      console.log(errorMessage);
+      // Reload the page after showing the error for a few seconds (e.g., 5 seconds)
+      setTimeout(reloadPage, 3000);
+    }
   };
   const inputStyles = `mb-5 w-full rounded-lg bg-primary-300
     px-5 py-3 placeholder-white`;
@@ -65,68 +79,73 @@ const Login = () => {
   return (
     <div>
       <h1>LOGIN</h1>
-      <div className="mt-10 justify-between gap-8 md:flex">
-        <motion.div
-          className="mt-10 basis-3/5 md:mt-0"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, y: 50 },
-            visible: { opacity: 1, y: 0 },
-          }}
-        >
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
+      {errorMessage && <div>Error: {errorMessage}</div>}
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="mt-10 justify-between gap-8 md:flex">
+          <motion.div
+            className="mt-10 basis-3/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
           >
-            <Form>
-              {
-                <div>
-                  <div className={inputStyles}>
-                    <label>Room ID: </label>
-                    <Field
-                      name="roomId"
-                      type="text"
-                      className="form-control"
-                      placeholder="room id"
-                    />
-                    <ErrorMessage
-                      name="roomId"
-                      component="div"
-                      className="alert alert-danger"
-                    />
-                  </div>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={handleSubmit}
+            >
+              <Form>
+                {
+                  <div>
+                    <div className={inputStyles}>
+                      <label>Room ID: </label>
+                      <Field
+                        name="roomId"
+                        type="text"
+                        className="form-control"
+                        placeholder="room id"
+                      />
+                      <ErrorMessage
+                        name="roomId"
+                        component="div"
+                        className="alert alert-danger"
+                      />
+                    </div>
 
-                  <div className={inputStyles}>
-                    <label>Password: </label>
-                    <Field
-                      name="roomPassword"
-                      type="password"
-                      className="form-control"
-                      placeholder="password"
-                    />
-                    <ErrorMessage
-                      name="roomPassword"
-                      component="div"
-                      className="alert alert-danger"
-                    />
-                  </div>
+                    <div className={inputStyles}>
+                      <label>Password: </label>
+                      <Field
+                        name="roomPassword"
+                        type="password"
+                        className="form-control"
+                        placeholder="password"
+                      />
+                      <ErrorMessage
+                        name="roomPassword"
+                        component="div"
+                        className="alert alert-danger"
+                      />
+                    </div>
 
-                  <button
-                    type="submit"
-                    className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
-                  >
-                    LOGIN
-                  </button>
-                </div>
-              }
-            </Form>
-          </Formik>
-        </motion.div>
-      </div>
+                    <button
+                      type="submit"
+                      className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+                    >
+                      LOGIN
+                    </button>
+                  </div>
+                }
+              </Form>
+            </Formik>
+          </motion.div>
+        </div>
+      )}
     </div>
   );
 };
