b66ee0fe2eb41d5325cd6980a50e9b131ae47768
Yeo Meng Han <yeomenghan1989@gmail.com>
2023-07-23 00:09:55 +0800

    removed proxy

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -5,13 +5,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": "https://blueberry-production.up.railway.app",
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": "https://blueberry-production.up.railway.app",
+  //   },
+  // },
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
